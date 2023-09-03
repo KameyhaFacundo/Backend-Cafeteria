3fe1b6fe94f996a1b28a1ce1c43dd50223d09583
Facundo Kameyha <kameyhafacundo@gmail.com>
2023-09-03 18:23:04 -0300

    Agregamos un puerto a un backend

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 // creo una variable con express
 
 app.set("port", process.env.PORT || 4000); // el puerto de mi servidor es el mismo
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto " + app.get("port"));
+});
 
 // 2 - middlewares
 
